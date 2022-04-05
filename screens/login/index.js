aea834af181547f806beb7f89b3424db855a3721
Daniel Ik <codingeinstein@gmail.com>
2022-04-05 11:56:10 +0100

    added index.js

---
+++ screens/login/index.js
@@ -0,0 +1,47 @@
+import React from "react";
+import styles from "./styles";
+
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require("./assets/login/loginbg.png")}
+        style={styles.img}
+      ></ImageBackground>
+
+      <Text style={styles.welcomeText}>Welcome Back!</Text>
+
+      <View style={styles.loginButtons}>
+        <View style={styles.googleLogin}>
+          <View style={styles.textLogo}>
+            <Text style={styles.googleText}>Login with Google</Text>
+
+            <View>
+              <ImageBackground
+                source={require("./assets/login/googleicon.png")}
+                style={styles.Logo}
+              ></ImageBackground>
+            </View>
+          </View>
+        </View>
+
+        <View style={styles.facebookLogin}>
+          <View style={styles.textLogo}>
+            <Text style={styles.fbtwText}>Login with Facebook</Text>
+            <View style={styles.Logo}></View>
+          </View>
+        </View>
+
+        <View style={styles.twitterLogin}>
+          <View style={styles.textLogo}>
+            <Text style={styles.fbtwText}>Login with Twitter</Text>
+            <View style={styles.Logo}></View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+
+export default Login;
