901f4ccce70f5aacd5c7a9136df1b31be98a4618
Daniel Ik <codingeinstein@gmail.com>
2022-04-07 13:17:10 +0100

    loginsignup page

---
+++ screens/loginsignup/index.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import {View,Text,Image} from 'react-native';
+import styles from './styles';
+import CustomBtn from '../../components/buttons';
+
+const LoginSignup = ()=>{
+
+    return (
+
+        <View style={styles.container}>
+            
+            <Image style={styles.img}/>
+
+
+            <View style={styles.Btns}>
+
+                <CustomBtn 
+                        btnStyle={styles.loginBtn}
+                        textStyle={styles.loginTextStyle}
+                        text={"Log in"}/>
+
+                
+                <CustomBtn 
+                        btnStyle={styles.signupBtn}
+                        textStyle={styles.signupTextStyle}
+                        text={"Sign up"}/>
+
+            </View>
+
+
+
+        </View>
+
+    )
+};
+
+export default LoginSignup;
