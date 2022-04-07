6000830ec73788e294cd08851d9fb031398dfb5b
Daniel Ik <codingeinstein@gmail.com>
2022-04-07 09:04:27 +0100

    added index.js

--- screens/callHome/index.js
+++ screens/callHome/index.js
@@ -1,10 +1,62 @@
 import React from 'react';
 import {View,Text} from 'react-native';
-import styles from './styles'
+import CustomBtn from '../../components/buttons';
+import styles from './styles';
+import RecentDetail from './components';
+
+const recents = [
+    {
+        link: "xcjwjerm",
+        timeStamp: "Just Now",
+        rejoinable:true,
+    },
+    {
+        link: "xcjwjerm",
+        timeStamp: "Just Now",
+        rejoinable:false,
+    },
+    {
+        link: "xcjwjerm",
+        timeStamp: "Just Now",
+        rejoinable:true,
+    },
+]
 
 const CallHome = ()=>{
     return (
+        <View style={styles.container}>
 
+            <View style={styles.callBtns}>
+
+                <CustomBtn 
+                    btnStyle={styles.newCallBtn}
+                    text={"New call"}
+                    textStyle={styles.newCallBtnText}
+                    />
+
+                <CustomBtn 
+                    btnStyle={styles.joinCallBtn}
+                    text={"Join call"}
+                    textStyle={styles.joinCallBtnText}
+                    />
+
+            </View>
+
+            <Text style={styles.recentHeader}>Recent Calls</Text>
+
+            <View style={styles.recentCalls}>
+
+            {
+
+                recents.map((el)=>{
+                    return <RecentDetail link={el.link} timeStamp={el.timeStamp} rejoinable={el.rejoinable}/>
+                })
+            } 
+
+
+            </View>
+
+        </View>
     );
 };
 
