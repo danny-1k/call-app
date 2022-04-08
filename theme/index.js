73c74674ae0675f426f9c1863dbdf23e8bfec851
Daniel Ik <codingeinstein@gmail.com>
2022-04-08 13:23:55 +0100

    colors

---
+++ theme/index.js
@@ -0,0 +1,13 @@
+const colors = {
+    white:'#ffffff',
+    black:'#333333',
+    gray :'#777777',
+    blue :'#03256C',
+    purple:'#AF25DF',
+    cyan:'#06BEE1',
+    twitterBlue:'#1DA1F2',
+    facebookBlue:'#0148A4',
+
+}
+
+export default colors;
