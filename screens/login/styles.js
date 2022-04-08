f83d67a187c7626f8c915c2dbcda6dbf4cb24edb
Daniel Ik <codingeinstein@gmail.com>
2022-04-08 12:56:00 +0100

    updated styles

--- screens/login/styles.js
+++ screens/login/styles.js
@@ -2,16 +2,46 @@ import React from "react";
 import { StyleSheet } from "react-native";
 
 const styles = StyleSheet.create({
+
+  btn: {
+    height: 60,
+    width: "100%",
+    marginTop: 30,
+    borderRadius: 5,
+    borderColor: "#655F5F",
+    backgroundColor:'white',
+    justifyContent:'center',
+
+    // box shadow for both android & ios
+    
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 1 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,  
+    elevation: 5
+
+  },
+  btnText: {
+    fontSize: 15,
+    marginLeft:15,
+    color: "#000",
+    marginTop:5,
+
+  },
+
   container: {
     flex: 1,
     backgroundColor: "#fff",
+    display:'flex',
+
   },
 
   loginButtons: {
-    marginBottom: 104,
+    marginBottom: 'auto',
     alignSelf:'center',
     width: 240,
     marginTop: "auto",
+    justifyContent:'center',
   },
 
   img: {
@@ -20,56 +50,17 @@ const styles = StyleSheet.create({
     marginTop: 90,
     resizeMode:'contain',
   },
+
   welcomeText: {
     fontSize: 30,
     marginTop: 9,
     fontFamily: "Roboto",
     marginLeft: 30,
   },
-  googleLogin: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#fff",
-    borderRadius: 5,
-    borderColor: "#655F5F",
-    borderWidth: 1,
-  },
-  facebookLogin: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#395796",
-    marginTop: 30,
-    borderRadius: 5,
-    borderColor: "#655F5F",
-  },
-  twitterLogin: {
-    height: 60,
-    width: "100%",
-    backgroundColor: "#1DA1F2",
-    marginTop: 30,
-    borderRadius: 5,
-    borderColor: "#655F5F",
-  },
 
-  googleText: {
-    color: "#333",
-    fontSize: 15,
-    marginLeft: 15,
-    marginTop: 20,
-    marginBottom: 20,
-  },
 
-  fbtwText: {
-    color: "#fff",
-    fontSize: 15,
-    marginLeft: 15,
-    marginTop: 20,
-    marginBottom: 20,
-  },
-
-  textLogo: {
+  innerBtnContainer: {
     display: "flex",
-    flex: 1,
     justifyContent: "space-between",
     flexDirection:'row',
   },
@@ -77,8 +68,8 @@ const styles = StyleSheet.create({
   Logo: {
     width: 30,
     height: 30,
-    marginRight:16,
-    alignSelf:'center',
+    marginRight:15,
+
   },
 });
 
