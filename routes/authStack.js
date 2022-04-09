02f229010efb2a9e47e673be7ef499d1ee23babd
Daniel Ik <codingeinstein@gmail.com>
2022-04-09 10:27:16 +0100

    updated authStack.js

--- routes/authStack.js
+++ routes/authStack.js
@@ -1,3 +1,27 @@
-import { createNativeStackNavigator } from "@react-navigation/stack";
-import {createAppContainer} from "react-navigation";
+// import { createNativeStackNavigator } from "@react-navigation/stack";
+// import {createAppContainer} from "react-navigation";
 
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LoginSignup from "../screens/loginsignup";
+import Login from "../screens/login";
+
+
+const screens = {
+    LoginSignup: {
+        screen: LoginSignup,
+
+    },
+
+    Login: {
+        screen: Login,
+
+    },
+
+};
+
+const AuthStack = createNativeStackNavigator(screens);
+
+export default NavigationContainer(AuthStack);
