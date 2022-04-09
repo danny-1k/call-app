72b1a09db473dd33bba19abd6b13ad0c53a86ac8
Daniel Ik <codingeinstein@gmail.com>
2022-04-09 21:41:22 +0100

    styles for joincall screen

---
+++ screens/joincall/styles.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { StyleSheet, Platform} from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        flex:1,
+        backgroundColor:'#2541B2',
+    },
+
+    textInputContainer:{
+        marginTop:100,
+
+    },
+
+    callCodeInput:{
+        marginHorizontal:15,
+        height:45,
+        backgroundColor:'#DDD',
+        borderRadius:5,
+        padding:15,
+        marginTop:15,
+
+    },
+
+    text: {
+        marginHorizontal:15,
+        fontSize:20,
+        color:'#fff',
+    },
+    
+});
+
+export default styles;
