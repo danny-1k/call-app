5981b053dd2e2195a0be22fe3ae222f970141f4e
Daniel Ik <codingeinstein@gmail.com>
2022-04-11 15:27:06 +0100

    fixed bugs

--- firebase.js
+++ firebase.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
-import * as firebase from "firebase";
-import { getAnalytics } from "firebase/analytics";
+import firebase from "firebase/compat/app";
+import { getAnalytics } from "firebase/compat/analytics";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,7 +17,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig)
+const app = firebase.initializeApp(firebaseConfig)
             ?(firebase.apps.length === 0)
             :firebase.app();
 
