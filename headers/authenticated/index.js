2c78f27c7183c0aec27ac0c99d80a911fd149af1
Daniel Ik <codingeinstein@gmail.com>
2022-04-11 18:38:23 +0100

    updated

--- headers/authenticated/index.js
+++ headers/authenticated/index.js
@@ -1,3 +1,21 @@
 import React from "react";
-import {View,Text} from "react-native";
-import Styles from './styles'
+import {View,Text,Image} from "react-native";
+import styles from "./styles";
+
+
+const Header = ()=>{
+
+    return (
+        <View style={styles.header}>
+            <Text style={styles.menu}>Menu</Text>
+            
+            <View>
+                <Image source={require("../../assets/call/profilepic.jpg")} style={styles.profilepic}/>
+
+            </View>
+        </View>
+    );
+
+};
+
+export default Header;
--- headers/authenticated/styles.js
+++ headers/authenticated/styles.js
@@ -3,7 +3,27 @@ import { StyleSheet } from 'react-native';
 
 
 const styles = StyleSheet.create({
-    
+    header: {
+        width:'100%',
+        height:'100%',
+        display:'flex',
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        // marginHorizontal:15,
+
+    },
+    menu: {
+        // marginLeft:15,
+    },
+
+    profilepic: {
+        marginRight:15,
+        width:40,
+        height:40,
+        borderRadius:20,
+    },
+
 });
 
 export default styles;
