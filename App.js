665399466e8f592c2e1ab33a40036a7d9a5d649f
Daniel Ik <codingeinstein@gmail.com>
2022-04-11 18:37:41 +0100

    updated App.js

--- App.js
+++ App.js
@@ -1,17 +1,11 @@
-import Login from './screens/login';
-import Call from './screens/call';
-import CallHome from './screens/callHome';
-import LoginSignup from './screens/loginsignup';
-
-import AuthStack from './routes/authStack';
+import AuthRoute from './routes/authRoute';
+import CallRoute from './routes/callRoute';
 
 export default function App() {
   return (
 
-    // <Login />
-    <Call/> 
-    // <CallHome/>
-    // <LoginSignup/>
+    <AuthRoute/>
+    // <CallRoute/>
     
   )
 }
