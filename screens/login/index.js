e798de733de79afec6767f379d14ab89d7bf21d6
Daniel Ik <codingeinstein@gmail.com>
2022-04-12 17:33:54 +0100

    updated login index.js

--- screens/login/index.js
+++ screens/login/index.js
@@ -8,29 +8,15 @@ import colors from '../../theme';
 
 import firebase,{ auth } from "../../firebase";
 
-import * as Google from 'expo-auth-session/providers/google';
+import * as WebBrowser from 'expo-web-browser';
+import {request,response,promptAsync,signin} from '../../authentication';
 
+WebBrowser.maybeCompleteAuthSession();
 
 const Login = () => {
 
-  const [request, response, promptAsync] = Google.useAuthRequest({
-    expoClientId:'944920655043-molf225ngchkh33p4hkh6rst94nqmu35.apps.googleusercontent.com',
-    webClientId: '944920655043-molf225ngchkh33p4hkh6rst94nqmu35.apps.googleusercontent.com',
-  });
 
-  useEffect(()=>{
-    if(response?.type === 'success'){
-      const {authentication} = response;
-    }
-
-    }
-    ,[response])
-
-  
-
-  const handleLoginWithTwitter = () => {
-
-  };
+  useEffect(signin,[response]);
 
 
 
@@ -57,19 +43,9 @@ const Login = () => {
           imageSource={require("../../assets/login/googleicon.png")}
           iconStyle={styles.Logo}
           disabled={!request}
-          onPress={()=>promptAsync()}
+          onPress={promptAsync}
           text={"Log in with Google"}
           textStyle={{...styles.btnText,...{color:colors.black,}}}
-        />
-
-        <BtnWithIcon 
-          btnStyle={{...styles.btn,...{backgroundColor:colors.twitterBlue,}}}
-          textIconStyle={styles.innerBtnContainer}
-          imageSource={require("../../assets/login/twittericon.png")}
-          iconStyle={styles.Logo}
-          onPress={()=>{console.warn('Clicked on signin with twitter btn')}}
-          text={"Log in with Twitter"}
-          textStyle={{...styles.btnText,...{color:colors.white,}}}
         />
 
       </View>
