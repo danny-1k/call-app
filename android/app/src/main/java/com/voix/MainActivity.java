90ba04a2ab909f19dc9493ef99ddb9cb6a2f635c
Daniel Ik <codingeinstein@gmail.com>
2022-04-23 23:23:46 +0100

    stupid react-screens fix

--- android/app/src/main/java/com/voix/MainActivity.java
+++ android/app/src/main/java/com/voix/MainActivity.java
@@ -3,6 +3,9 @@ package com.voix;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
+import android.os.Bundle;
+
+
 
 public class MainActivity extends ReactActivity {
 
@@ -13,6 +16,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "voix";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState){
+    super.onCreate(null);
   }
 
   /**
