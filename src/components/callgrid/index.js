d0ca8634263a8dd3355a1d827aaebfce60827cb5
Daniel Ik <codingeinstein@gmail.com>
2022-04-23 23:06:27 +0100

    added index.js

---
+++ src/components/callgrid/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {View, Image} from 'react-native';
+
+const CallGrid = ({num,dim})=>{
+
+    const cell_dim = Math.floor(((dim**2)/findNearestSquare(num))**.5);
+
+    return (
+
+        <View style={{width:dim,height:dim,flexWrap:'wrap',flexDirection:'column',justifyContent:'space-around',}}>
+
+            <View style={{flexWrap:'wrap',flexDirection:'row', alignSelf:'center'}}>
+
+                {
+                [...Array(num)].map(
+                    (e,i)=>{
+                        return <Image key={i} style={{width:cell_dim,height:cell_dim,}} source={require('./../../assets/call/profilepic.jpg')}></Image>
+                    })
+                }
+            </View>
+
+            
+
+        </View>
+
+    );
+};
+
+export default CallGrid;
