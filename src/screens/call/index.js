72036595550461b28a81d129d33d3099cd85c941
Daniel Ik <codingeinstein@gmail.com>
2022-04-23 23:07:42 +0100

    moved CallGrid into separate file

--- src/screens/call/index.js
+++ src/screens/call/index.js
@@ -4,46 +4,8 @@ import CustomBtn, {BtnWithIcon} from '../../components/buttons';
 
 import styles from './styles';
 
-const isSquare = (num)=>{
-    return (num**.5)-Math.floor(num**.5) === 0;
-};
+import CallGrid from '../../components/callgrid';
 
-const findNearestSquare = (num)=>{
-    while (true){
-        if(isSquare(num)){
-            return num;
-        };
-
-        num+=1
-
-    };
-};
-
-
-const CallGrid = ({num,dim})=>{
-
-    const cell_dim = Math.floor(((dim**2)/findNearestSquare(num))**.5);
-
-    return (
-
-        <View style={{width:dim,height:dim,flexWrap:'wrap',flexDirection:'column',justifyContent:'space-around',}}>
-
-            <View style={{flexWrap:'wrap',flexDirection:'row', alignSelf:'center'}}>
-
-                {
-                [...Array(num)].map(
-                    (e,i)=>{
-                        return <Image key={i} style={{width:cell_dim,height:cell_dim,}} source={require('./../../assets/call/profilepic.jpg')}></Image>
-                    })
-                }
-            </View>
-
-            
-
-        </View>
-
-    );
-}
 
 
 const Call = ()=>{
