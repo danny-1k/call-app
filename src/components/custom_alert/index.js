cfd0627e5e0a9f345ea2e6da71be67672165ad5c
Daniel Ik <codingeinstein@gmail.com>
2022-04-24 21:35:07 +0100

    updated index.js

--- src/components/custom_alert/index.js
+++ src/components/custom_alert/index.js
@@ -1,8 +1,38 @@
 import React,{useState} from 'react';
+import {Modal,Pressable} from 'react-native';
 import styles from './styles';
 
 
-const Alert = ()=>{
+const Alert = (props)=>{
+    const [isVisible,setIsVisible] = useState(false);
+
+    return (
+        <View>
+            <Modal 
+                animationType='fade' 
+                transparent={true} 
+                visible={isVisible} 
+                onRequestClose={()=>{
+                    setIsVisible(!isVisible);
+            }}>
+
+                <Pressable style={styles.backDrop} onPress={()=>{setIsVisible(false)}} />
+
+                <View style={styles.alertBox}>
+
+                    <View style={styles.alertBoxContainer}>
+                        <Text style={styles.alertBoxTitle}>{props.title || 'Alert'}</Text>
+                        <Text style={styles.alertBoxMessage}>{props.message || ''}</Text>
+
+
+                    </View>
+
+                </View>
+
+
+            </Modal>
+        </View>
+    );
 
 };
 
