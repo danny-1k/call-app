4f769a8f96d51defa006afc41c4c000563a94a2d
Daniel Ik <codingeinstein@gmail.com>
2022-04-24 22:00:33 +0100

    updated styles.js

--- src/components/custom_alert/styles.js
+++ src/components/custom_alert/styles.js
@@ -18,12 +18,12 @@ const styles = StyleSheet.create({
     },
 
     alertBoxContainer: {
-        backgroundColor:'#333',
+        backgroundColor:'#fafafa',
         maxWidth:280,
         width:'100%',
         margin:48,
         elevation:24,
-        borderRadius:2,
+        borderRadius:5,
     },
 
     alertBoxTitle: {
@@ -31,6 +31,7 @@ const styles = StyleSheet.create({
         fontFamily:'initial',
         fontSize:24,
         fontWeight:'bold',
+        margin:24,
 
     },
 
@@ -39,7 +40,35 @@ const styles = StyleSheet.create({
         fontFamily:'initial',
         fontSize:16,
         fontWeight:'normal',
+        marginLeft:24,
+        marginRight:24,
+        marginBottom:24,
     },
+
+    alertButtons: {
+        marginTop:0,
+        marginRight:0,
+        marginBottom:8,
+        marginLeft:24,
+        flexDirection:'row',
+        justifyContent:'flex-end',
+    },
+
+    alertButton: {
+        marginTop:12,
+        marginRight:8,
+        padding:10,
+        elevation:2,
+        // alignSelf:'flex-end',
+
+    },
+
+    alertButtonText:{
+        fontWeight:'500',
+        fontSize:16,
+        textTransform:'uppercase',
+        color:'#387ef5',
+    }
 
 });
 
