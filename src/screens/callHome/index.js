44726be90ba366ac1fe23d9b79b3d17d72e1c740
Daniel Ik <codingeinstein@gmail.com>
2022-04-29 21:30:30 +0100

    updated index.js

--- src/screens/callHome/index.js
+++ src/screens/callHome/index.js
@@ -1,89 +1,7 @@
 import React from 'react';
-import {View,Text,ScrollView} from 'react-native';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
-import CustomBtn from '../../components/buttons';
-import styles from './styles';
-import RecentDetail from './components';
-
-
-const recents = [
-    {   
-        id:0,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:true,
-    },
-    {
-        id:1,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-    {
-        id:2,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-    {
-        id:3,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:true,
-    },
-    {
-        id:4,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:true,
-    },
-    {   
-        id:5,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-    {
-        id:6,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:true,
-    },
-    {
-        id:7,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-    {
-        id:8,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-    {
-        id:9,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:true,
-    },
-    {
-        id:10,
-        link: "xcjwjerm",
-        timeStamp: "Just Now",
-        rejoinable:false,
-    },
-];
-
-
-const SettingsScreen = ({navigation}) =>{
-    return (
-        <View>
-            <Text>Settings page</Text>
-        </View>
-    );
-};
+import { HomeScreen, SettingsScreen } from './screens';
 
 
 const Drawer = createDrawerNavigator();
@@ -96,54 +14,13 @@ const CallHome = ({navigation})=>{
             <Drawer.Navigator initialRouteName='Home'>
 
                 <Drawer.Screen name='Home' component={HomeScreen}/>
+                <Drawer.Screen name='Settings' component={SettingsScreen}/>
+
 
             </Drawer.Navigator>
 
         </NavigationContainer>
 
-        <View style={styles.container}>
-
-            <ScrollView showsVerticalScrollIndicator={false}>
-
-
-                <View style={styles.callBtns}>
-
-                    <CustomBtn 
-                        btnStyle={styles.newCallBtn}
-                        text={"New call"}
-                        textStyle={styles.newCallBtnText}
-                        />
-
-                    <CustomBtn 
-                        btnStyle={styles.joinCallBtn}
-                        text={"Join call"}
-                        textStyle={styles.joinCallBtnText}
-                        onPress={()=>navigation.navigate('JoinCall')}
-                        />
-
-                </View>
-
-
-
-
-                <Text style={styles.recentHeader}>Recent Calls</Text>
-
-                <View style={styles.recentCalls}>
-
-                    {
-
-                        recents.map((el)=>{
-                            return <RecentDetail link={el.link} timeStamp={el.timeStamp} rejoinable={el.rejoinable} key={el.id}/>
-                        })
-
-                    }
-                </View>
-                
-
-            </ScrollView>
-
-
-        </View>
     );
 };
 
