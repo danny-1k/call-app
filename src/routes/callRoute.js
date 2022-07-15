083d1132cc74be7d5110ce7a1fc68fdd0e61bb75
Daniel Ik <codingeinstein@gmail.com>
2022-07-15 20:43:44 +0100

    updated callRoute.js

--- src/routes/callRoute.js
+++ src/routes/callRoute.js
@@ -1,22 +1,81 @@
-import React,{useState} from 'react';
+import React,{useState, useContext} from 'react';
 
-import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
 
 import CallHome from "../screens/callHome";
 import JoinCall from "../screens/joincall";
 
-// import Header from "../headers/authenticated";
 import CustomBtn from '../components/buttons';
-import { Button , StatusBar,StyleSheet} from 'react-native';
-
+import { StatusBar, Image} from 'react-native';
+import {AuthContext} from '../components/auth';
 
 
 const Stack = createNativeStackNavigator();
 
 
-const Home = ()=>{
+const Home = ({isValid, setIsValid, setJoinCode})=>{
+
+
+    const ProfilePic = ()=>{
+
+        const {authData} = useContext(AuthContext);
+        console.log(authData.photo)
+
+        return (
+            <Image source={{uri:authData.photo}} style={{width:40,height:40,borderRadius:50}}/>
+        );
+
+
+
+    };
+
+
+
+    const headerJoinBtnStyle = {
+
+        valid:{
+            backgroundColor:'#03256C',
+            width:75,
+            height:45,
+            borderRadius:5,
+            alignItems:'center',
+            justifyContent:'center',
+
+
+
+        },
+
+        invalid: {
+            backgroundColor:'#ddd',
+            borderColor:'rgba(0,0,0,.4)',
+            width:75,
+            height:45,
+            borderRadius:5,
+            alignItems:'center',
+            justifyContent:'center',
+            
+
+        },
+        
+    
+    };
+    
+    
+    const headerJoinTextStyle = {
+        valid: {
+            fontSize:20,
+            color:'#fff',
+
+
+        },
+        invalid: {
+            fontSize:20,
+            color:'rgba(0,0,0,.4)',
+
+        }
+    
+    
+    };
 
 
     const options = {
@@ -35,6 +94,10 @@ const Home = ()=>{
         headerStyle: {
             backgroundColor:'#2541B2'
         },
+
+        headerRight: ()=>(    
+            <ProfilePic/>
+        ),
     
         ...options,
     
@@ -55,7 +118,7 @@ const Home = ()=>{
         headerTitleStyle: {
             color:'#fff',
         },
-    
+
         
         headerRight: ()=>(
             <CustomBtn 
@@ -71,27 +134,28 @@ const Home = ()=>{
 
     return (
 
-        <>
+                <>
+                    <StatusBar backgroundColor={'#2541B2'}/>
 
-            <StatusBar backgroundColor={'#2541B2'}/>
-
-            <NavigationContainer>
-
-                <Stack.Navigator initialRouteName="CallHome">
-                    <Stack.Screen name="CallHome" options={callHomeOptions} component={CallHome}
+                    <Stack.Navigator initialRouteName="CallHome">
+                        <Stack.Screen
+                            name="CallHome"
+                            options={callHomeOptions}
+                            component={CallHome}
                         />
 
 
-                    <Stack.Screen name="JoinCall" options={joinCallOptions}>
+                        <Stack.Screen name="JoinCall" options={joinCallOptions}>
 
-                        {(props) => <JoinCall {...props} setIsValid={setIsValid} setJoinCode={setJoinCode}/> }
+                            {(props) => <JoinCall {...props} setIsValid={setIsValid} setJoinCode={setJoinCode}/> }
 
-                    </Stack.Screen>
+                        </Stack.Screen>
 
 
-                </Stack.Navigator>
-            </NavigationContainer>
-        </>
+                    </Stack.Navigator>
+
+                
+                </>
 
     );
 };
@@ -111,14 +175,11 @@ const CallRoute = ()=>{
 
     return (
 
-        <Home/>
+        <Home isValid={isValid} setIsValid={setIsValid} setJoinCode={setJoinCode}/>
         
         
     );
 };
-
-// const styles = sty
-
 
 
 export default CallRoute;
