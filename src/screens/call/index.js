b76cb7b3f66a9ec047bb92c10fcc27a446858fed
Daniel Ik <codingeinstein@gmail.com>
2022-07-16 17:22:39 +0100

    added bottomSheet and Vector Icons

--- src/screens/call/index.js
+++ src/screens/call/index.js
@@ -1,6 +1,13 @@
-import React from 'react';
-import {View,Text,Image} from 'react-native';
-import CustomBtn, {BtnWithIcon} from '../../components/buttons';
+import React, { useRef} from 'react';
+import {View,Text, Image} from 'react-native';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+import BottomSheet, {BottomSheetView, BottomSheetScrollView} from '@gorhom/bottom-sheet';
+
+import FA_Icon  from 'react-native-vector-icons/FontAwesome';
+import MC_Icon  from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+import {ButtonWrapper} from '../../components/buttons';
 
 import styles from './styles';
 
@@ -8,7 +15,11 @@ import CallGrid from '../../components/callgrid';
 
 
 
-const Call = ()=>{
+const Call = gestureHandlerRootHOC(()=>{
+
+    const bottomSheetRef = useRef(null);
+    const snapPoints = [130,'60%'];
+
 
     return (
 
@@ -17,36 +28,73 @@ const Call = ()=>{
 
         <View style={styles.callParticipantsContainer}>
 
-        <CallGrid num={1} dim={300}/>
+            <CallGrid num={1} dim={300}/>
 
         </View>
 
 
-        <View style={styles.callButtons}>
+        <BottomSheet ref={bottomSheetRef} index={0} snapPoints={snapPoints} backgroundStyle={{backgroundColor:'rgba(29,29,29,.5)'}}>
+            
+            <BottomSheetView style={styles.callButtons}> 
 
-                <BtnWithIcon btnStyle={styles.muteBtn}
-                    imageSource={require("../../assets/call/mic.png")}
-                    iconStyle={styles.mtnBtnIcon}
-                    onPress={()=>{console.warn('Clicked on mute btn')}}/>
+                <ButtonWrapper onPress={()=>{console.warn('You pressed me')}} btnStyle={{...styles.speaker, ...styles.callBtn}}>
+                    <FA_Icon name='volume-up' size={40} color='#fff'/>
+                </ButtonWrapper>
 
-                <BtnWithIcon btnStyle={styles.endCallBtn}
-                    imageSource={require("../../assets/call/hang_up.png")}
-                    iconStyle={styles.endCallBtnIcon}
-                    onPress={()=>{console.warn('Clicked on call btn')}}/>
+                <ButtonWrapper onPress={()=>{console.warn('You pressed me')}} btnStyle={{...styles.mute, ...styles.callBtn}}>
+                    <FA_Icon name='microphone-slash' size={40} color='#fff'/>
+                </ButtonWrapper>
 
-                <CustomBtn 
-                    btnStyle={styles.leaveCallBtn} 
-                    text={"Leave call"} 
-                    textStyle={styles.leaveText} 
-                    onPress={()=>{console.warn('Clicked on leave btn')}}/>
+                <ButtonWrapper onPress={()=>{console.warn('You pressed me')}} btnStyle={{...styles.hangup, ...styles.callBtn}}>
+                    <MC_Icon name='phone-hangup' size={40} color='#fff'/>
+                </ButtonWrapper>
+
+            </BottomSheetView>
+
+            <BottomSheetScrollView>
+
+                <Text style={{color:'#fff', fontSize:25, marginTop:10, marginLeft:20, marginBottom:30, }}>
+                    Participants
+                </Text>
 
 
-        </View>
+
+                <View style={{marginBottom:20,marginLeft:20}}>
+                    <View style={{ flexDirection:'row', alignItems:'center'}}>
+                        <Image source={require('../../assets/call/profilepic.jpg')} style={{height:50, width:50, borderRadius:25}} />
+                        <Text style={{color:'#ccc', fontSize:20, marginLeft:30}}>Daniel Ik</Text>
+                    </View>
+                </View>
+
+                <View style={{marginBottom:20,marginLeft:20}}>
+                    <View style={{ flexDirection:'row', alignItems:'center'}}>
+                        <Image source={require('../../assets/call/profilepic.jpg')} style={{height:50, width:50, borderRadius:25}} />
+                        <Text style={{color:'#ccc', fontSize:20, marginLeft:30}}>Daniel Ik</Text>
+                    </View>
+                </View>
+
+                <View style={{marginBottom:20,marginLeft:20}}>
+                    <View style={{ flexDirection:'row', alignItems:'center'}}>
+                        <Image source={require('../../assets/call/profilepic.jpg')} style={{height:50, width:50, borderRadius:25}} />
+                        <Text style={{color:'#ccc', fontSize:20, marginLeft:30}}>Daniel Ik</Text>
+                    </View>
+                </View>
+
+                <View style={{marginBottom:20,marginLeft:20}}>
+                    <View style={{ flexDirection:'row', alignItems:'center'}}>
+                        <Image source={require('../../assets/call/profilepic.jpg')} style={{height:50, width:50, borderRadius:25}} />
+                        <Text style={{color:'#ccc', fontSize:20, marginLeft:30}}>Daniel Ik</Text>
+                    </View>
+                </View>
+
+            </BottomSheetScrollView>
+
+        </BottomSheet>
         
 
     </View>
     )
 
-};
+});
 
 export default Call;
