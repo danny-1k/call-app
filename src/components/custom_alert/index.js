3dea6a594e2c9982005a287db763bee88ad3dd24
Daniel Ik <codingeinstein@gmail.com>
2022-07-29 10:25:22 +0100

    added Info component

--- src/components/custom_alert/index.js
+++ src/components/custom_alert/index.js
@@ -1,7 +1,9 @@
 import React,{useState} from 'react';
 import {Modal,Pressable,View,Text} from 'react-native';
 import styles from './styles';
-import CustomBtn from '../buttons';
+import CustomBtn, {ButtonWrapper} from '../buttons';
+import FA_Icon  from 'react-native-vector-icons/FontAwesome';
+
 
 
 const Alert = (props)=>{
@@ -42,6 +44,48 @@ const Alert = (props)=>{
         </View>
     );
 
+};
+
+export const Info = (props) =>{
+    const isVisible = props.isVisible;
+    const setIsVisible = props.setIsVisible;
+    const onClose = props.onClose;
+
+    return (
+        <View>
+            <Modal 
+                animationType='fade' 
+                transparent={true} 
+                visible={isVisible} 
+                onRequestClose={()=>{
+                    setIsVisible(!isVisible);
+            }}>
+
+                <Pressable style={styles.backDrop} onPress={()=>{onClose();setIsVisible(false)}} />
+
+                <View style={styles.alertBox}>
+
+                    <View style={styles.alertBoxContainer}>
+                        <View>
+                            
+                            <ButtonWrapper onPress={()=>{onClose();setIsVisible(false)}} btnStyle={styles.infoCloseButton}>
+                                <FA_Icon name='close' size={20} color='gray'/>
+                            </ButtonWrapper>
+
+                        </View>
+                        <View style={styles.alertBoxMessage}>
+                            {props.children}
+                        </View>
+
+
+                    </View>
+
+                </View>
+
+
+            </Modal>
+        </View>
+    );
 };
 
 export default Alert;
